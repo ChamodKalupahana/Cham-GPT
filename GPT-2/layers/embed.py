0fc588791a832a38b93b8a7a906b5509c569cff5
root <chamodkalupahana@gmail.com>
2026-01-29 22:56:52 +0000

    started emed

--- GPT-2/layers/embed.py
+++ GPT-2/layers/embed.py
@@ -1,15 +1,18 @@
 import torch
 import torch.nn as nn
 
+from torch import Tensor
+
 device = t.device(
     "mps" if t.backends.mps.is_available() else "cuda" if t.cuda.is_available() else "cpu"
 )
 
 class Embed(nn.Module):
-    def __init__(self, cfg):
-        # self.W_E = nn.Parameter()
+    def __init__(self, num_embeddings : int, embedding_dim : int):
+        weights = t.rand(num_embeddings, embedding_dim)
+        self.W_E = nn.Parameter(weights, requires_grad=True)
         return
     
-    def forward(self, input):
+    def forward(self, input : Tensor) -> Tensor:
         return 
     
